5ed4e733fe2a0c9921f7b13295d5bed57c6a6a9b
Valentin <89931955+valentronus95@users.noreply.github.com>
2026-08-18 09:17:16 +0300

    Add R13 bundled weapon variant spawn subsystem

---
+++ OsterConflict/Source/OsterConflict/Public/OCR13WeaponVariantSpawnSubsystem.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "OCR13WeaponVariantSpawnSubsystem.generated.h"
+
+/** Exposes bundled R13 weapon models as real world-pickup gameplay variants without changing starter loadouts. */
+UCLASS()
+class OSTERCONFLICT_API UOCR13WeaponVariantSpawnSubsystem : public UWorldSubsystem
+{
+    GENERATED_BODY()
+
+public:
+    virtual bool ShouldCreateSubsystem(UObject* Outer) const override;
+    virtual void OnWorldBeginPlay(UWorld& InWorld) override;
+
+private:
+    void SpawnBundledVariants(UWorld& World);
+};
