35390303580cb9e2c3ba56484009a75da54ceb28
Valentin <89931955+valentronus95@users.noreply.github.com>
2026-08-20 21:42:19 +0300

    R14: add dedicated HMMWV spawn point type

--- OsterConflict/Source/OsterConflict/Public/OCCombatVehicleSpawnPoints.h
+++ OsterConflict/Source/OsterConflict/Public/OCCombatVehicleSpawnPoints.h
@@ -4,12 +4,22 @@
 #include "OCVehicleSpawnPoint.h"
 #include "OCCombatVehicleSpawnPoints.generated.h"
 
+/** Production armed pickup spawn point. */
 UCLASS()
 class OSTERCONFLICT_API AOCPickupGunTruckSpawnPoint : public AOCVehicleSpawnPoint
 {
     GENERATED_BODY()
 public:
     AOCPickupGunTruckSpawnPoint();
+};
+
+/** Production HMMWV + M2 spawn point. */
+UCLASS()
+class OSTERCONFLICT_API AOCHMMWVGunTruckSpawnPoint : public AOCVehicleSpawnPoint
+{
+    GENERATED_BODY()
+public:
+    AOCHMMWVGunTruckSpawnPoint();
 };
 
 UCLASS()
