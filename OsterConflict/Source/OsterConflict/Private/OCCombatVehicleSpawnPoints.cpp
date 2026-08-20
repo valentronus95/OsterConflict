41b5d5a6218b6bd22f851097702bf2e4718f77e0
Valentin <89931955+valentronus95@users.noreply.github.com>
2026-08-20 21:42:28 +0300

    R14: wire distinct pickup and HMMWV vehicle classes

--- OsterConflict/Source/OsterConflict/Private/OCCombatVehicleSpawnPoints.cpp
+++ OsterConflict/Source/OsterConflict/Private/OCCombatVehicleSpawnPoints.cpp
@@ -1,10 +1,17 @@
 #include "OCCombatVehicleSpawnPoints.h"
 #include "OCPickupGunTruck.h"
+#include "OCHMMWVGunTruck.h"
 #include "OCBTR.h"
 
 AOCPickupGunTruckSpawnPoint::AOCPickupGunTruckSpawnPoint()
 {
     VehicleClass = AOCPickupGunTruck::StaticClass();
+    RespawnDelaySeconds = 48.0f;
+}
+
+AOCHMMWVGunTruckSpawnPoint::AOCHMMWVGunTruckSpawnPoint()
+{
+    VehicleClass = AOCHMMWVGunTruck::StaticClass();
     RespawnDelaySeconds = 48.0f;
 }
 
