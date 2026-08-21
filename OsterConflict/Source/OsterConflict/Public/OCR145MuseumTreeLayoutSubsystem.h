1561295ebcd3a1e207bdb4be14a90af6be366c0a
Valentin <89931955+valentronus95@users.noreply.github.com>
2026-08-21 19:03:00 +0300

    Expose immediate museum tree layout stage

--- OsterConflict/Source/OsterConflict/Public/OCR145MuseumTreeLayoutSubsystem.h
+++ OsterConflict/Source/OsterConflict/Public/OCR145MuseumTreeLayoutSubsystem.h
@@ -4,6 +4,8 @@
 #include "Subsystems/WorldSubsystem.h"
 #include "OCR145MuseumTreeLayoutSubsystem.generated.h"
 
+class UWorld;
+
 /** Replaces the symmetric R13.7 museum tree seeds with the asymmetrical photo-oriented site layout. */
 UCLASS()
 class OSTERCONFLICT_API UOCR145MuseumTreeLayoutSubsystem : public UWorldSubsystem
@@ -13,6 +15,8 @@ class OSTERCONFLICT_API UOCR145MuseumTreeLayoutSubsystem : public UWorldSubsyste
 public:
     virtual bool ShouldCreateSubsystem(UObject* Outer) const override;
     virtual void OnWorldBeginPlay(UWorld& InWorld) override;
+
+    void RunAuthoritativeDetailNow(UWorld& World) { ReplaceMuseumTrees(World); }
 
 private:
     void ReplaceMuseumTrees(UWorld& World) const;
