f648c267dadfeaa1128d79bba4d4275e75a9e79c
Valentin <89931955+valentronus95@users.noreply.github.com>
2026-08-21 19:02:09 +0300

    Expose immediate museum window stage

--- OsterConflict/Source/OsterConflict/Public/OCR141MuseumWindowReplacementSubsystem.h
+++ OsterConflict/Source/OsterConflict/Public/OCR141MuseumWindowReplacementSubsystem.h
@@ -4,6 +4,8 @@
 #include "Subsystems/WorldSubsystem.h"
 #include "OCR141MuseumWindowReplacementSubsystem.generated.h"
 
+class UWorld;
+
 /** Replaces only museum prototype breakable windows with the photo-styled museum variant. */
 UCLASS()
 class OSTERCONFLICT_API UOCR141MuseumWindowReplacementSubsystem : public UWorldSubsystem
@@ -13,6 +15,8 @@ class OSTERCONFLICT_API UOCR141MuseumWindowReplacementSubsystem : public UWorldS
 public:
     virtual bool ShouldCreateSubsystem(UObject* Outer) const override;
     virtual void OnWorldBeginPlay(UWorld& InWorld) override;
+
+    void RunAuthoritativeDetailNow(UWorld& World) { ReplaceMuseumWindows(World); }
 
 private:
     void ReplaceMuseumWindows(UWorld& World) const;
