dc94e1e3e2e47ddce19286188edf662360924cfe
Valentin <89931955+valentronus95@users.noreply.github.com>
2026-08-21 19:01:38 +0300

    Expose immediate museum door stage

--- OsterConflict/Source/OsterConflict/Public/OCR139MuseumMainDoorReplacementSubsystem.h
+++ OsterConflict/Source/OsterConflict/Public/OCR139MuseumMainDoorReplacementSubsystem.h
@@ -4,6 +4,8 @@
 #include "Subsystems/WorldSubsystem.h"
 #include "OCR139MuseumMainDoorReplacementSubsystem.generated.h"
 
+class UWorld;
+
 /** Replaces the two generic R13.8 main-door leaves with the photo-proportioned museum double door. */
 UCLASS()
 class OSTERCONFLICT_API UOCR139MuseumMainDoorReplacementSubsystem : public UWorldSubsystem
@@ -13,6 +15,8 @@ class OSTERCONFLICT_API UOCR139MuseumMainDoorReplacementSubsystem : public UWorl
 public:
     virtual bool ShouldCreateSubsystem(UObject* Outer) const override;
     virtual void OnWorldBeginPlay(UWorld& InWorld) override;
+
+    void RunAuthoritativeDetailNow(UWorld& World) { ReplaceMainDoor(World); }
 
 private:
     void ReplaceMainDoor(UWorld& World) const;
