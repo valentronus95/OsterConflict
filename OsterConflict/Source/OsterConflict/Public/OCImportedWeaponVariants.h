cc3c8a4b006e84da98439daa7ba713041da9f629
Valentin <89931955+valentronus95@users.noreply.github.com>
2026-09-04 16:57:49 +0300

    Pass45: declare all imported weapon identities

---
+++ OsterConflict/Source/OsterConflict/Public/OCImportedWeaponVariants.h
@@ -0,0 +1,93 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "OCAntiArmorLauncher.h"
+#include "OCWeaponBase.h"
+#include "OCImportedWeaponVariants.generated.h"
+
+/**
+ * Distinct gameplay identities for weapon models already imported by the user.
+ * Visuals are resolved from their exact local Fab/content package by the PASS45 imported-weapon bridge.
+ * No unrelated model may substitute for a missing identity.
+ */
+
+UCLASS()
+class OSTERCONFLICT_API AOCWeapon_AK74M : public AOCWeaponBase
+{
+    GENERATED_BODY()
+public:
+    AOCWeapon_AK74M();
+    virtual void BeginPlay() override;
+};
+
+UCLASS()
+class OSTERCONFLICT_API AOCWeapon_AR15 : public AOCWeaponBase
+{
+    GENERATED_BODY()
+public:
+    AOCWeapon_AR15();
+    virtual void BeginPlay() override;
+};
+
+UCLASS()
+class OSTERCONFLICT_API AOCWeapon_M4A1 : public AOCWeaponBase
+{
+    GENERATED_BODY()
+public:
+    AOCWeapon_M4A1();
+    virtual void BeginPlay() override;
+};
+
+UCLASS()
+class OSTERCONFLICT_API AOCWeapon_FnBallista : public AOCWeaponBase
+{
+    GENERATED_BODY()
+public:
+    AOCWeapon_FnBallista();
+    virtual void BeginPlay() override;
+};
+
+UCLASS()
+class OSTERCONFLICT_API AOCWeapon_Kar98k : public AOCWeaponBase
+{
+    GENERATED_BODY()
+public:
+    AOCWeapon_Kar98k();
+    virtual void BeginPlay() override;
+};
+
+UCLASS()
+class OSTERCONFLICT_API AOCWeapon_Makarov : public AOCWeaponBase
+{
+    GENERATED_BODY()
+public:
+    AOCWeapon_Makarov();
+    virtual void BeginPlay() override;
+};
+
+UCLASS()
+class OSTERCONFLICT_API AOCWeapon_TommyGun : public AOCWeaponBase
+{
+    GENERATED_BODY()
+public:
+    AOCWeapon_TommyGun();
+    virtual void BeginPlay() override;
+};
+
+UCLASS()
+class OSTERCONFLICT_API AOCWeapon_M72LAW : public AOCAntiArmorLauncher
+{
+    GENERATED_BODY()
+public:
+    AOCWeapon_M72LAW();
+    virtual void BeginPlay() override;
+};
+
+UCLASS()
+class OSTERCONFLICT_API AOCWeapon_RPG26 : public AOCAntiArmorLauncher
+{
+    GENERATED_BODY()
+public:
+    AOCWeapon_RPG26();
+    virtual void BeginPlay() override;
+};
