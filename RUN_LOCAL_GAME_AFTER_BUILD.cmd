61f83ad56bdb1966fcb6eb3d989cb71987f0d059
Valentin <89931955+valentronus95@users.noreply.github.com>
2026-09-04 11:23:28 +0300

    OPS: remove redundant local game launch wrapper

--- RUN_LOCAL_GAME_AFTER_BUILD.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0PC_TEST\LAUNCH_LATEST_LOCAL_SESSION.ps1"
-set RC=%ERRORLEVEL%
-echo.
-echo Local launch exit code: %RC%
-pause
-exit /b %RC%
