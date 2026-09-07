55bd40c24ecd7d5965d453197f3a52f516a07efe
Valentin <89931955+valentronus95@users.noreply.github.com>
2026-09-07 15:55:59 +0300

    perf(game recovery): prepare async launcher production visual

--- OsterConflict/Source/OsterConflict/Public/OCAntiArmorLauncher.h
+++ OsterConflict/Source/OsterConflict/Public/OCAntiArmorLauncher.h
@@ -2,6 +2,9 @@
 #include "CoreMinimal.h"
 #include "OCWeaponBase.h"
 #include "OCAntiArmorLauncher.generated.h"
+
+class FStreamableHandle;
+
 UCLASS()
 class OSTERCONFLICT_API AOCAntiArmorLauncher : public AOCWeaponBase
 {
@@ -9,9 +12,13 @@ class OSTERCONFLICT_API AOCAntiArmorLauncher : public AOCWeaponBase
 public:
     AOCAntiArmorLauncher();
     virtual void BeginPlay() override;
+    virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
     virtual bool TryFireServer(AOCCharacter* Shooter, const FVector& TraceOrigin, const FVector& TraceDirection,
         bool bAiming, bool bMoving, FHitResult& OutHit, bool& bOutDamagedActor, bool& bOutFatalHit) override;
 private:
+    void CompleteProductionVisualPreload();
+
+    TSharedPtr<FStreamableHandle> ProductionVisualPreloadHandle;
     double LastLauncherFireTime = -1000.0;
     int32 LauncherAudioEventCounter = 0;
 };
